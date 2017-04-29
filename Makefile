6edc2de3962785adac5e80ed843c0581a0888c0c
VicdeJuan <dejuan.victor@gmail.com>
2017-04-29 19:08:57 +0200

    Marco teórico terminado II

--- Makefile
+++ Makefile
@@ -1,7 +1,17 @@
 all: TFM.pdf
 
-TFM.pdf: TFM.tex
+graphs: img/*.txt
+	dot -Tpng circuloVicioso.txt -o circuloVicioso.png 
+
+R: src/*.R
+	Rscript src/*.R
+
+
+TFM.pdf: TFM.tex tex/* img/* src/* memoria.bib
 	pdflatex TFM.tex; bibtex TFM; makeglossaries TFM; makeindex TFM; pdflatex TFM.tex; pdflatex TFM.tex
 
+bib: memoria.bib
+	bibtex TFM; pdflatex TFM.tex;
+	
 clean:
-	rm -f TFM.dvi TFM.ps *.snm *.out *.nav *.log *.aux *.toc *.vrb *.pdf *~ *.lof *.blg *.bbl
+	rm -f TFM.dvi TFM.ps *.snm *.out *.nav *.log *.aux *.toc *.vrb *.pdf *~ *.lof *.blg *.bbl *.acn *.acr *.alg *.glg *.glo *.gls *.glsdefs *.idx *.ilg *.ind *.ist *.lot *.tmp *.xdy
--- src/plotPisa.R
+++ src/plotPisa.R
@@ -1,31 +1,39 @@
 setwd('/home/vicdejuan/Documents/Master/TFM/src')
 library(ggplot2)
 
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
 
 rez=read.csv('pisa.csv')[c(1,2,3)]
 ex=read.csv('pisa.csv')[c(1,4,5)]
 
+color1=3
+color2=7
+
 pex = ggplot(ex,aes(x=factor(Year))) + 
     geom_bar(aes(y=OCDE.1,fill="Media OCDE"),stat="identity") + 
-    geom_line(aes(y=Espana.1,color="España"),group = 1)+ 
-    scale_colour_manual(" ", values=c("Media OCDE" = cbPalette[3], "España" = cbPalette[6]))+
-    scale_fill_manual("",values=cbPalette[3])+
+    geom_line(aes(y=Espana.1,color="España"),group = 2,size=1.2)+ 
+    geom_point(aes(y=Espana.1,color="España"),group = 1,size=3)+
+    scale_colour_manual(" ", values=c("Media OCDE" = cbPalette[color1], "España" = cbPalette[color2]))+
+    scale_fill_manual("",values=cbPalette[color1])+
     xlab("Año") + ylab("Porcentaje excelentes") + 
     theme(legend.key=element_blank(),
         legend.title=element_blank(),
-        axis.text.y = element_text(face="bold", 
-                                   size=12))
-ggsave("../img/PisaExcelentes.png")
+        axis.text=element_text(size=15,face="bold"),
+        axis.title=element_text(size=14,face="bold"))
+        
+ggsave("../img/PisaExcelentes.png",height=6,width=8)
 
 prez = ggplot(rez,aes(x=factor(Year))) + 
   geom_bar(aes(y=OCDE,fill="Media OCDE"),stat="identity") + 
-  geom_line(aes(y=Espana,color="España"),group = 1)+ 
-  scale_colour_manual(" ", values=c("Media OCDE" = cbPalette[3], "España" = cbPalette[6]))+
-  scale_fill_manual("",values=cbPalette[3])+
+  geom_line(aes(y=Espana,color="España"),group = 2,size=1.2)+ 
+  geom_point(aes(y=Espana,color="España"),group = 1,size=3)+
+  scale_colour_manual(" ", values=c("Media OCDE" = cbPalette[color1], "España" = cbPalette[color2]))+
+  scale_fill_manual("",values=cbPalette[color1])+
   xlab("Año") + ylab("Porcentaje rezagados") + 
   theme(legend.key=element_blank(),
         legend.title=element_blank(),
-        axis.text.y = element_text(face="bold", 
-                                   size=12))
+        axis.title=element_text(size=14,face="bold"),
+        axis.text=element_text(size=15,face="bold"))
 
-ggsave("../img/PisaRezagados.png")
+ggsave("../img/PisaRezagados.png",height=6,width=8)
